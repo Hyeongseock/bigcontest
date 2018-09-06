d0c0cfcb1d5e25f6584497595abdc9825d9deefa
jja2wa <jja2wa@gmail.com>
2018-09-06 10:20:12 +0900

    전처리 테스트

--- csv_utils.py
+++ csv_utils.py
@@ -23,3 +23,28 @@ def getTrainPayment(path):
 ## train_activity
 def getTrainTrade(path):
     return pd.read_csv(path+'train/train_trade.csv')
+
+
+## test_activity
+def getTestActivity(path):
+    return pd.read_csv(path+'test/test_activity.csv')
+
+## test_label
+def getTestLabel(path):
+    return pd.read_csv(path+'test/test_label.csv')
+
+## test_guild
+def getTestGuild(path):
+    return pd.read_csv(path+'test/test_guild.csv')
+
+## test_party
+def getTestParty(path):
+    return pd.read_csv(path+'test/test_party.csv')
+
+## test_payment
+def getTestPayment(path):
+    return pd.read_csv(path+'test/test_payment.csv')
+
+## test_activity
+def getTestTrade(path):
+    return pd.read_csv(path+'test/test_trade.csv')
--- main(jh).py
+++ main(jh).py
@@ -1,15 +1,76 @@
 import pre_activity_data as act
 import pre_guild_data as gld
+import pre_payment as pay
 import csv_utils
 import logging as log
 import pandas as pd
 import mask
 log.basicConfig(level=log.DEBUG)
 
+# def rf_train(data) :
+#     label = data["label"]
+#     label = pd.get_dummies(label, drop_first=True)
+#
+#     independent_variable = data[["member_cnt"]]
+#
+#     #print('Training Features Shape:', independent_variable.shape)
+#     #print('Training Labels Shape:', label.shape)
+#
+#     train_data, test_data, train_label, test_label = \
+#         train_test_split(independent_variable, label)
+#     clf_rf = RandomForestClassifier(bootstrap=True, class_weight=None, criterion='gini',
+#                                     max_depth=None, max_features="auto", max_leaf_nodes=None,
+#                                     min_impurity_decrease=0.0, min_samples_leaf=1, min_samples_split=2,
+#                                     min_weight_fraction_leaf=0.0, n_estimators=10, n_jobs=1,
+#                                     oob_score=False, random_state=None, verbose=0, warm_start=False)
+#     #print('Training Features Shape:', train_data.shape)
+#     #print('Training Labels Shape:', train_label.shape)
+#     #print('Training Features Shape:', test_data.shape)
+#     #print('Training Labels Shape:', test_label.shape)
+#     clf_rf.fit(train_data, train_label)
+#     rf_pre = clf_rf.predict(test_data)
+#     as_score_rf = metrics.accuracy_score(test_label, rf_pre)
+#     print(as_score_rf*100)
+#
+
 def main() :
+    pd.DataFrame = mask.apply_masks()
+    pri_key = "acc_id"
     ######################################### 전처리 시작 #########################################
     # 파일 경로
     root_path = "E:/NCsoft/champion_data/"
+    train_label_data = csv_utils.getTrainLabel(root_path)
+
+    # week_data = train_label_data.set_index('label').filter(like="week", axis=0)
+    # week_data['week_val']=1
+    # retained_data = train_label_data.set_index('label').filter(like="retained", axis=0)
+    # retained_data['retained_val'] = 1
+    # month_data = train_label_data.set_index('label').filter(regex="^month", axis=0)
+    # month_data['month_val'] = 1
+    # month2_data = train_label_data.set_index('label').filter(regex="2month", axis=0)
+    # month2_data['month2_val'] = 1
+    #
+    # train_label_data = gld.concat_data(train_label_data, week_data)
+    # train_label_data = gld.concat_data(train_label_data, retained_data)
+    # train_label_data = gld.concat_data(train_label_data, month_data)
+    # train_label_data = gld.concat_data(train_label_data, month2_data)
+    #
+    # train_label_data.fillna(0, inplace=True)
+    #
+    # new_label = "E:/NCsoft/champion_data/train_preprocessing/new_train_label"
+    # gld.save_file(train_label_data, new_label)
+
+
+
+    # test_label_data = csv_utils.getTestLabel(root_path)
+    #
+    # final_label_data = pd.merge(test_label_data, train_label_data, how='left', on='acc_id')
+    # print(final_label_data.head())
+
+
+
+    # new_label = "E:/NCsoft/champion_data/result/result_label"
+    # gld.save_file(final_label_data, new_label)
 
     #train_activity 데이터 전처리 하기
     # train_activity_data = train_activity_preprocessing(root_path)
@@ -19,17 +80,56 @@ def main() :
 
     # train_guild 데이터 전처리 하기
     # train_guild_data = train_guild_preprocessing(root_path)
-
+    #
     # new_guild = "E:/NCsoft/champion_data/train_preprocessing/new_train_guild"
     # gld.save_file(train_guild_data, new_guild)
+
+    # train_payment 데이터 전처리 하기
+    train_payment_data = train_payment_preprocessing(root_path)
+
+    new_payment = "E:/NCsoft/champion_data/train_preprocessing/new_train_payment"
+    gld.save_file(train_payment_data, new_payment)
     ######################################### 전처리 끝 #########################################
 
+
+#train_payment 데이터 전처리 하기
+def train_payment_preprocessing(root_path) :
+    # train_payment 원본
+    train_payment_data = csv_utils.getTrainPayment(root_path)
+    train_label_data = pd.read_csv(root_path+'train_preprocessing/new_train_label.csv')
+
+    train_payment_data["cnt"] = 1
+    # new_data1 = pd.DataFrame(train_payment_data.set_index('payment_amount').filter(like="149898470874251", axis=0).groupby("acc_id").sum()['cnt']).reset_index()
+    new_data1 = pd.DataFrame(train_payment_data.groupby("acc_id").sum()['cnt']).reset_index()
+    print(new_data1.size)
+    new_data0 = pd.DataFrame(train_payment_data.set_index('payment_amount').filter(like="149898470874251", axis=0).groupby("acc_id").sum()['cnt']).reset_index()
+    new_data0['cnt'] = 8 - new_data0['cnt']
+
+    new_data2 = pd.DataFrame(train_payment_data.groupby("acc_id").mean()['payment_amount']).reset_index()
+    new_data3 = pd.DataFrame(train_payment_data.groupby("acc_id").max()['payment_amount']).reset_index()
+    new_data4 = pd.DataFrame(train_payment_data.groupby("acc_id").min()['payment_amount']).reset_index()
+    # new_data0.drop(["acc_id"], axis=1, inplace=True)
+    new_data2.drop(["acc_id"], axis=1, inplace=True)
+    new_data3.drop(["acc_id"], axis=1, inplace=True)
+    new_data4.drop(["acc_id"], axis=1, inplace=True)
+    # data = pd.concat([new_data4, new_data1], axis=1)
+    data = pd.merge(new_data1, new_data0, on="acc_id", how="left").fillna(0, inplace=True)
+    data = pd.concat([data, new_data2], axis=1)
+    data = pd.concat([data, new_data3], axis=1)
+    data = pd.concat([data, new_data4], axis=1)
+    data = pd.merge(data, train_label_data, on="acc_id", how="left")
+
+
+    # pay_data = pay.pre_payment(train_payment_data, train_label_data)  # 유저(acc_id)별로 총 8주 중 몇 주를 방문했는지 cnt하는 함수
+
+
+    return data
 
 # train_guild 데이터 전처리 하기
 def train_guild_preprocessing(root_path):
     # train_data 전처리하기
     train_guild_data = csv_utils.getTrainGuild(root_path)
-    train_label_data = csv_utils.getTrainLabel(root_path)
+    train_label_data = pd.read_csv(root_path+'train_preprocessing/new_train_label.csv')
 
     guild_data = gld.get_guild_member_cnt(train_guild_data)
     guild_member_avg = gld.get_guild_member_avg(guild_data)
